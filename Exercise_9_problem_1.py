7a1be584d92e3ad749cbbb4e69c19087c0247ff0
YukaTakahashi15 <82014372+YukaTakahashi15719048@users.noreply.github.com>
2021-06-12 06:21:16 +0000

    I plotted the polygon

--- Exercise_9_problem_1.py
+++ Exercise_9_problem_1.py
@@ -51,7 +51,7 @@ print(poly.geom_type)
 # Create an empty GeoDataFrame
 geo = None
 # YOUR CODE HERE 3
-##create the dataframe called "geo" with a new column named 'geometry'
+##create the dataframe called "geo" with new column named 'geometry'
 geo = gpd.GeoDataFrame(index=[0], columns=['geometry'])
 geo['geometry'] = poly
 # CODE FOR TESTING YOUR SOLUTION
@@ -70,7 +70,8 @@ print(len(geo))
 
 # Plot the polygon. What shape is it :) ?
 # YOUR CODE HERE 4
-
+##plot the polygon
+geo.plot()
 # What can you see on the map? :) 
 
 # - save the GeoDataFrame into a Shapefile called `'polygon.shp'`. 
@@ -79,7 +80,8 @@ print(len(geo))
 fp = 'polygon.shp'
 
 # YOUR CODE HERE 5
-
+#save
+geo.to_file(fp)
 # CODE FOR TESTING YOUR SOLUTION
 
 #Check if output file exists
